22352de89d6de676d9f6fcc58e56cd705645c61d
Matt Drees <matt.drees@cru.org>
2013-09-09 16:37:22 -0400

    split datasource creation / destruction logic out
    make a new DatasourceManager class that is responsible for creating or looking up the
    maintenance datasource, depending on configuration parameters

--- maintenance-filter-server/src/main/java/org/ccci/maintenance/Bootstrap.java
+++ maintenance-filter-server/src/main/java/org/ccci/maintenance/Bootstrap.java
@@ -1,32 +1,23 @@
 package org.ccci.maintenance;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.SQLException;
-import java.util.Properties;
-
-import javax.naming.InitialContext;
-import javax.naming.NameNotFoundException;
-import javax.naming.NamingException;
-import javax.servlet.ServletContext;
-import javax.sql.DataSource;
-
 import org.ccci.maintenance.util.Clock;
 import org.ccci.maintenance.util.ConfigReader;
-import org.ccci.maintenance.util.Exceptions;
-import org.h2.jdbcx.JdbcConnectionPool;
+
+import javax.servlet.ServletContext;
+import javax.sql.DataSource;
 
 public class Bootstrap
 {
 
     private final ServletContext servletContext;
     private final ConfigReader configReader;
-    private JdbcConnectionPool pool;
+    private DatasourceManager datasourceManager;
 
     public Bootstrap(ServletContext servletContext)
     {
         this.servletContext = servletContext;
         this.configReader = new ConfigReader(servletContext, System.getProperties());
+        this.datasourceManager = new DatasourceManager(configReader);
     }
 
     public static Bootstrap getInstance(ServletContext servletContext)
@@ -50,7 +41,7 @@ public class Bootstrap
         MaintenanceServiceImpl maintenanceService;
         if (bootstrap == null)
         {
-            DataSource dataSource = lookupOrCreateDataSource();
+            DataSource dataSource = datasourceManager.lookupOrCreateDataSource();
             initDatabaseIfNecessary(dataSource);
             servletContext.setAttribute(bootstrapLocation, this);
 
@@ -82,168 +73,12 @@ public class Bootstrap
     /** may be called multiple times, if multiple filters are configured */
     public void shutdown()
     {
-        shutdownPoolIfNecessary();
-    }
-
-    private void shutdownPoolIfNecessary()
-    {
-        if (pool != null)
-        {
-            try
-            {
-                pool.dispose();
-                pool = null;
-            }
-            catch (SQLException e)
-            {
-                throw Exceptions.wrap(e);
-            }
-        }
+        datasourceManager.shutdownPoolIfNecessary();
     }
 
     private void initDatabaseIfNecessary(DataSource dataSource)
     {
         new DatabaseMigrator(dataSource).migrate();
-    }
-
-    private DataSource lookupOrCreateDataSource()
-    {
-        String datasourceParamName = "org.ccci.maintenance.window.datasource";
-        String dbPathParamName = "org.ccci.maintenance.window.db.path";
-        
-        String datasourceLocation = configReader.getParameter(datasourceParamName);
-        if (datasourceLocation != null)
-        {
-            return lookupDataSource(datasourceLocation);
-        }
-        else
-        {
-            String dbPath = configReader.getParameter(dbPathParamName);
-            if (dbPath == null)
-            {
-                dbPath = getDbPathFromConfigFileOrFail(datasourceParamName, dbPathParamName);
-            }
-            initH2DatasourcePoolLocatedAt(dbPath);
-            return pool;
-        }
-    }
-
-    private String getDbPathFromConfigFileOrFail(String datasourceParamName, String dbPathParamName) {
-        String configFileNameParamName = "org.ccci.maintenance.window.db.path.configfile.name";
-        String configFilePropertyKeyParamName = "org.ccci.maintenance.window.db.path.configfile.propertykey";
-
-        String configFile = configReader.getParameter(configFileNameParamName);
-        if (configFile == null)
-        {
-            throw new IllegalArgumentException(String.format(
-                "you must provide either %s, %s, or %s  as an init-param",
-                datasourceParamName,
-                dbPathParamName,
-                configFileNameParamName
-            ));
-        }
-        else
-        {
-            String dbPathPropertyKey = configReader.getParameter(configFilePropertyKeyParamName);
-            if (dbPathPropertyKey == null)
-                throw new IllegalArgumentException(String.format(
-                    "you must provide %s when you use %s",
-                    configFilePropertyKeyParamName,
-                    configFileNameParamName
-                ));
-            return getDatabasePathFromConfigFile(configFile, dbPathPropertyKey);
-        }
-    }
-
-    private String getDatabasePathFromConfigFile(String configFile, String dbPathPropertyKey)
-    {
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream configFileStream = classLoader.getResourceAsStream(configFile);
-        if (configFileStream == null)
-            throw new IllegalStateException(String.format(
-                "Cannot find config file resource '%s' from classloader %s", 
-                configFile,
-                classLoader));
-        Properties configProperties = loadConfigProperties(configFileStream);
-        
-        String dbPath = configProperties.getProperty(dbPathPropertyKey);
-        if (dbPath == null)
-            throw new IllegalStateException(String.format(
-                "config file '%s' doesn't define a property with key %s", 
-                configFile,
-                dbPathPropertyKey));
-            
-        return dbPath;
-    }
-
-    private Properties loadConfigProperties(InputStream configFileStream)
-    {
-        try
-        {
-            Properties config = new Properties();
-            try
-            {
-                config.load(configFileStream);
-            }
-            catch (IOException e)
-            {
-                throw Exceptions.wrap(e);
-            }
-            return config;
-        }
-        finally
-        {
-            closeStreamCarefully(configFileStream);
-        }
-    }
-
-    private void closeStreamCarefully(InputStream configFileStream)
-    {
-        try
-        {
-            configFileStream.close();
-        }
-        catch (IOException e)
-        {
-            Exceptions.swallow(e, "exception closing config stream");
-        }
-    }
-
-    private void initH2DatasourcePoolLocatedAt(String dbPath)
-    {
-        /*
-         * use a file-based database, since this must persist across jvm restarts.
-         */
-        String url = "jdbc:h2:file:" + dbPath;
-        pool = JdbcConnectionPool.create(url, "sa", "");
-    }
-
-    private DataSource lookupDataSource(String datasourceLocation)
-    {
-        try
-        {
-            Object found = new InitialContext().lookup(datasourceLocation);
-            if (found == null)
-            {
-                throw new IllegalStateException("No datasource bound at " + datasourceLocation);
-            }
-            if (!(found instanceof DataSource))
-            {
-                throw new IllegalStateException(String.format(
-                    "Found %s bound at %s instead of a DataSource",
-                    found,
-                    datasourceLocation));
-            }
-            return (DataSource) found;
-        }
-        catch (NameNotFoundException e)
-        {
-            throw new IllegalStateException("No datasource bound at " + datasourceLocation, e);
-        }
-        catch (NamingException e)
-        {
-            throw Exceptions.wrap(e);
-        }
     }
 
     public MaintenanceService getMaintenanceService()
---
+++ maintenance-filter-server/src/main/java/org/ccci/maintenance/DatasourceManager.java
@@ -0,0 +1,188 @@
+package org.ccci.maintenance;
+
+import org.ccci.maintenance.util.ConfigReader;
+import org.ccci.maintenance.util.Exceptions;
+import org.h2.jdbcx.JdbcConnectionPool;
+
+import javax.naming.InitialContext;
+import javax.naming.NameNotFoundException;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @author Matt Drees
+ */
+public class DatasourceManager {
+
+    private ConfigReader configReader;
+    private JdbcConnectionPool pool;
+
+
+
+    String datasourceParamName = "org.ccci.maintenance.window.datasource";
+    String dbPathParamName = "org.ccci.maintenance.window.db.path";
+    String configFileNameParamName = "org.ccci.maintenance.window.db.path.configfile.name";
+    String configFilePropertyKeyParamName = "org.ccci.maintenance.window.db.path.configfile.propertykey";
+
+    public DatasourceManager(ConfigReader configReader) {
+        this.configReader = configReader;
+    }
+
+    public DataSource lookupOrCreateDataSource()
+    {
+
+        String datasourceLocation = configReader.getParameter(datasourceParamName);
+        if (datasourceLocation != null)
+        {
+            return lookupDataSource(datasourceLocation);
+        }
+        else
+        {
+            String dbPath = configReader.getParameter(dbPathParamName);
+            if (dbPath == null)
+            {
+                dbPath = getDbPathFromConfigFileOrFail();
+            }
+            initH2DatasourcePoolLocatedAt(dbPath);
+            return pool;
+        }
+    }
+
+    private String getDbPathFromConfigFileOrFail() {
+
+        String configFile = configReader.getParameter(configFileNameParamName);
+        if (configFile == null)
+        {
+            throw new IllegalArgumentException(String.format(
+                "you must provide either %s, %s, or %s  as an init-param",
+                datasourceParamName,
+                dbPathParamName,
+                configFileNameParamName
+            ));
+        }
+        else
+        {
+            String dbPathPropertyKey = configReader.getParameter(configFilePropertyKeyParamName);
+            if (dbPathPropertyKey == null)
+                throw new IllegalArgumentException(String.format(
+                    "you must provide %s when you use %s",
+                    configFilePropertyKeyParamName,
+                    configFileNameParamName
+                ));
+            return getDatabasePathFromConfigFile(configFile, dbPathPropertyKey);
+        }
+    }
+
+    private String getDatabasePathFromConfigFile(String configFile, String dbPathPropertyKey)
+    {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream configFileStream = classLoader.getResourceAsStream(configFile);
+        if (configFileStream == null)
+            throw new IllegalStateException(String.format(
+                "Cannot find config file resource '%s' from classloader %s",
+                configFile,
+                classLoader));
+        Properties configProperties = loadConfigProperties(configFileStream);
+
+        String dbPath = configProperties.getProperty(dbPathPropertyKey);
+        if (dbPath == null)
+            throw new IllegalStateException(String.format(
+                "config file '%s' doesn't define a property with key %s",
+                configFile,
+                dbPathPropertyKey));
+
+        return dbPath;
+    }
+
+    private Properties loadConfigProperties(InputStream configFileStream)
+    {
+        try
+        {
+            Properties config = new Properties();
+            try
+            {
+                config.load(configFileStream);
+            }
+            catch (IOException e)
+            {
+                throw Exceptions.wrap(e);
+            }
+            return config;
+        }
+        finally
+        {
+            closeStreamCarefully(configFileStream);
+        }
+    }
+
+    private void closeStreamCarefully(InputStream configFileStream)
+    {
+        try
+        {
+            configFileStream.close();
+        }
+        catch (IOException e)
+        {
+            Exceptions.swallow(e, "exception closing config stream");
+        }
+    }
+
+    private void initH2DatasourcePoolLocatedAt(String dbPath)
+    {
+        /*
+         * use a file-based database, since this must persist across jvm restarts.
+         */
+        String url = "jdbc:h2:file:" + dbPath;
+        pool = JdbcConnectionPool.create(url, "sa", "");
+    }
+
+    private DataSource lookupDataSource(String datasourceLocation)
+    {
+        try
+        {
+            Object found = new InitialContext().lookup(datasourceLocation);
+            if (found == null)
+            {
+                throw new IllegalStateException("No datasource bound at " + datasourceLocation);
+            }
+            if (!(found instanceof DataSource))
+            {
+                throw new IllegalStateException(String.format(
+                    "Found %s bound at %s instead of a DataSource",
+                    found,
+                    datasourceLocation));
+            }
+            return (DataSource) found;
+        }
+        catch (NameNotFoundException e)
+        {
+            throw new IllegalStateException("No datasource bound at " + datasourceLocation, e);
+        }
+        catch (NamingException e)
+        {
+            throw Exceptions.wrap(e);
+        }
+    }
+
+    public void shutdownPoolIfNecessary()
+    {
+        if (pool != null)
+        {
+            try
+            {
+                pool.dispose();
+                pool = null;
+            }
+            catch (SQLException e)
+            {
+                throw Exceptions.wrap(e);
+            }
+        }
+    }
+
+
+}
