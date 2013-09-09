33d8d47619fe3a8d52d8a691b54fd5687e19dab7
Matt Drees <matt.drees@cru.org>
2013-09-09 11:40:01 -0400

    add system property interpolation to ConfigReader

--- maintenance-filter-server/src/main/java/org/ccci/maintenance/util/ConfigReader.java
+++ maintenance-filter-server/src/main/java/org/ccci/maintenance/util/ConfigReader.java
@@ -2,6 +2,8 @@ package org.ccci.maintenance.util;
 
 import javax.servlet.ServletContext;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Matt Drees
@@ -10,6 +12,8 @@ public class ConfigReader {
     private ServletContext servletContext;
     private Properties properties;
 
+    static final Pattern pattern = Pattern.compile("\\{([^${}]*)\\}");
+
     public ConfigReader(ServletContext servletContext, Properties properties) {
         this.servletContext = servletContext;
         this.properties = properties;
@@ -17,6 +21,33 @@ public class ConfigReader {
 
     public String getParameter(String parameterName)
     {
-        return servletContext.getInitParameter(parameterName);
+        String literalParameter = servletContext.getInitParameter(parameterName);
+        if (literalParameter == null)
+            return null;
+        else
+            return interpolate(literalParameter);
+    }
+
+    private String interpolate(String value) {
+        String propertyReference = findPropertyReference(value);
+        if (propertyReference != null)
+            return interpolate(replacePropertyReference(value, propertyReference));
+        else
+            return value;
+    }
+
+    private String replacePropertyReference(String value, String propertyReference) {
+        String propertyValue = properties.getProperty(propertyReference);
+        if (propertyValue == null)
+            throw new IllegalArgumentException("there is no such system property: " + propertyReference);
+        return value.replace("${" + propertyReference + "}", propertyValue);
+    }
+
+    private String findPropertyReference(String value) {
+        Matcher matcher = pattern.matcher(value);
+        if (matcher.find())
+            return matcher.group(1);
+        else
+            return null;
     }
 }
